8a4017aa1d102d9b7649575361e7a64550619c5f
Luiz Ricardo Brugnera <luiz.r.brugnera@gmail.com>
2024-09-05 10:09:13 -0300

    fix: app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import { useState } from "react";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/pages/Login";
+import PrivateRoute from "./security/PrivateRoute";
+import { Home } from "./components/pages/Home";
+import Register from "./components/pages/Register";
 
-function App() {
-  return <></>;
-}
+const App: React.FC = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/home" element={<PrivateRoute component={Home} />} />
+    </Routes>
+  );
+};
 
 export default App;
